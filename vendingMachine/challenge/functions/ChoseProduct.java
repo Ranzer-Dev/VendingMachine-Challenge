556bfd8f7843cd45d98d8adcee9b06dc41eeba3f
Ranzer_Dev <rodolfo.antunes9@gmail.com>
2025-03-21 16:43:18 -0300

    removing some useless files

--- vendingMachine/challenge/functions/ChoseProduct.java
+++
@@ -1,5 +0,0 @@
-package vendingMachine.challenge.functions;
-
-public interface ChoseProduct {
-    public int chose(int chose);
-}
--- vendingMachine/challenge/functions/InsertCoin.java
+++
@@ -1,7 +0,0 @@
-package vendingMachine.challenge.functions;
-
-import java.util.Scanner;
-
-public interface InsertCoin {
-    double InsertCoin(double coin);
-}
